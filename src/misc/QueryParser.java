6648cb2829b1bb056300f1ad0d2e6af33c5af7d7
srikanth rao <srikanth.a.r@gmail.com>
2018-04-19 09:24:44 +0530

    Query Parsing

--- src/misc/QueryParser.java
+++ src/misc/QueryParser.java
@@ -1,28 +1,37 @@
 package misc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
-import java.util.function.IntPredicate;
-
 import misc.QueryParsingException.Type;
 
 public class QueryParser {
 
 	public static void main(String[] args) {
 		
-		String qe = "{timewindow:'3d',code:'ABCD',version:'01,00,0000',status:'completed'}";
+		String qe = "{timewindow:null,code:'ABCD',version:'01.00.0000',status:'abcd'}";
 		try{
-			System.out.println(parseQueryString(qe));
+			Map<String,BiValidator> keyValidatorPairs = new HashMap<String,BiValidator>();
+			keyValidatorPairs.put("status", (_key,_value)->Arrays.asList("completed","failed","aborted").contains(_value.toLowerCase()));
+			System.out.println(parseQueryString(qe,Arrays.asList("timewindow","code","version","status"),keyValidatorPairs));
 		}catch(QueryParsingException e){
-			System.out.println("Exception type : "+e.getType());
+			System.out.println("Exception : "+e.getMessage());
 			e.printStackTrace();
 		}
 	}
 	
+	
 	public static Map<String,String> parseQueryString(String queryExpression) throws QueryParsingException{
+		return parseQueryString(queryExpression,null,null);
+	}
+	public static Map<String,String> parseQueryString(String queryExpression, List<String> allowedKeys) throws QueryParsingException{
+		return parseQueryString(queryExpression,allowedKeys,null);
+	}
+	
+	public static Map<String,String> parseQueryString(String queryExpression,List<String> allowedKeys, Map<String,BiValidator> keyValidatorPairs) throws QueryParsingException{
 		try{
 			if(isNotNull(queryExpression) && startAndEndWithBraces(queryExpression)){
 				Map<String,String> _map = new HashMap<String,String>();
@@ -33,12 +42,19 @@ public class QueryParser {
 					token = token.trim();
 					
 					String[] l_and_r = token.split(":");
-					if(l_and_r.length != 2){
-						throw new QueryParsingException(Type.INVALID_SYNTAX);
-					}else if(!checkForMatchingQuotes(l_and_r[1].trim())){
-						throw new QueryParsingException(Type.MISMATCHED_QUOTES);
-					}else{
-						_map.put(l_and_r[0].trim(), l_and_r[1].trim());
+					String _key = l_and_r[0];
+					String _value = l_and_r[1];
+					if(validateKeyValuePair(_key, _value, allowedKeys)){
+						if(keyValidatorPairs!=null && keyValidatorPairs.containsKey(_key)){
+							BiValidator validator = keyValidatorPairs.get(_key);
+							if(validator.validate(_key, _value)){
+								_map.put(_key.trim(), _value.trim());
+							}else{
+								throw new QueryParsingException(Type.VALIDATOR_REJECTION, _key, _value);
+							}
+						}else{
+							_map.put(_key.trim(), _value.trim());
+						}
 					}
 				}
 				
@@ -49,33 +65,56 @@ public class QueryParser {
 		}catch(QueryParsingException qe){
 			throw qe;
 		}
-	}	
-	
-	private static List<String> split(String queryExpression){
-		int singleQuoteCount = (int) queryExpression.chars().filter(value -> value == 239).count();
-		List<String> retVal = new ArrayList<String>();
-		if(singleQuoteCount %2 == 0){
-			String [] arr = queryExpression.split("',");
-			for(String str : arr){
-				retVal.add(str+"'");
-			}
-		}
-		
-		return retVal;
-		
-//		new IntPredicate(){
-//
-//			@Override
-//			public boolean test(int value) {				
-//				return value == 239;
-//			}
-//			
-//		})
 	}
 	
-//	private static boolean isValidWithoutQuotes(String value){
-//		return !value.contains(" "); 
-//	}
+	private static boolean validateKeyValuePair(String key, String value, List<String> allowedKeys){
+		if(key == null || value == null){
+			throw new QueryParsingException(Type.INVALID_SYNTAX,key,value);
+		}else if(!checkForMatchingQuotes(value)){
+			if(value.chars().filter(_char -> _char == 39).count() %2!=0)
+				throw new QueryParsingException(Type.MISMATCHED_QUOTES,key,value);
+			else
+				throw new QueryParsingException(Type.NOT_STARTS_ENDS_WITH_BRACES,key,value);
+		}else if(allowedKeys!=null && !allowedKeys.contains(key)){
+			throw new QueryParsingException(Type.KEY_NOT_ALLOWED,key,value);
+		}
+		
+		return true;
+	}
+	
+	private static List<String> split(String queryExpression){
+		int quoteCount = (int)queryExpression.chars().filter(_char -> _char == 39).count();
+		
+		if(quoteCount%2!=0 || quoteCount==0){
+			throw new QueryParsingException(Type.MISMATCHED_QUOTES);
+		}
+		
+		String subStr = queryExpression;
+		for(int i=0;i<quoteCount;i++){
+			int beginIndex = subStr.indexOf("'");
+			if(beginIndex == -1)break;
+			subStr = subStr.substring(beginIndex+1);
+			
+			int endIndex = subStr.indexOf("'");
+			
+			String value = subStr.substring(0, endIndex);
+			if(value.contains(",")){
+				throw new QueryParsingException(Type.COMMA_NOT_ALLOWED_IN_VALUE);
+			}
+			
+			subStr = subStr.substring(endIndex+1);
+		}
+		
+		List<String> retVal = new ArrayList<String>();
+		
+		String [] arr = queryExpression.split(",");
+		for(String str : arr){
+			retVal.add(str);
+		}
+		
+		
+		return retVal;		
+	}
 	
 	private static boolean checkForMatchingQuotes(String value){
 		return startAndEndsWithSingleQuotes(value) ;//|| startAndEndsWithDoubleQuotes(value);
@@ -84,10 +123,6 @@ public class QueryParser {
 	private static boolean startAndEndsWithSingleQuotes(String value){
 		return value.startsWith("'") && value.endsWith("'"); 
 	}
-	
-//	private static boolean startAndEndsWithDoubleQuotes(String value){
-//		return value.startsWith("\"") && value.endsWith("\""); 
-//	}
 	
 	private static boolean isNotNull(String qe){
 		boolean retVal = qe!=null && qe.trim().length()>0;
--- src/misc/QueryParsingException.java
+++ src/misc/QueryParsingException.java
@@ -7,13 +7,27 @@ public class QueryParsingException extends RuntimeException{
 		NO_CONTENT,
 		INVALID_SYNTAX,
 		MISMATCHED_QUOTES,
-		INVALID_WITHOUT_QUOTES
+		INVALID_WITHOUT_QUOTES,
+		COMMA_NOT_ALLOWED_IN_VALUE,
+		KEY_NOT_ALLOWED,
+		VALIDATOR_REJECTION
 	}
 	
 	private Type type;
+	private String key;
+	private String value;
 	
 	public QueryParsingException(Type type) {
 		this.type = type;
+	}
+	public QueryParsingException(Type type, String key) {
+		this.type = type;
+		this.key = key;
+	}
+	public QueryParsingException(Type type, String key, String value) {
+		this.type = type;
+		this.value = value;
+		this.key = key;
 	}
 
 	public Type getType() {
@@ -22,5 +36,30 @@ public class QueryParsingException extends RuntimeException{
 
 	public void setType(Type type) {
 		this.type = type;
-	}	
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("Type").append(" : ").append(type).append(" , ");
+		sb.append("Key").append(" : ").append(key).append(" , ");
+		sb.append("Value").append(" : ").append(value);
+		return sb.toString();
+	}
+	
 }
